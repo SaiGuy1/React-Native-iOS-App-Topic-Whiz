410136159fee333ee756d5dfa102f4be5a58db8d
Sai <snarendran0304@gmail.com>
2016-09-15 19:01:09 -0700

    Implemented Input Fields for Email and Password

--- TopicWhiz/src/components/auth/signin.js
+++ TopicWhiz/src/components/auth/signin.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  TextInput,
+  TouchableOpacity,
   StyleSheet
 } from 'react-native';
 
 module.exports = React.createClass({
+  getInitialState() {
+    return({
+          email: '',
+          password: '',
+    })
+  },
+
   render() {
     return(
       <View style={styles.container}>
         <Text> Sign In </Text>
+        <TextInput
+          placeholder="Email"
+          style={styles.input}
+          onChangeText={(text) => this.setState({email: text})}
+        />
+
+        <TextInput
+          placeholder="Password"
+          style={styles.input}
+          onChangeText={(text) => this.setState({password: text})}
+        />
       </View>
     )
   }
@@ -19,6 +39,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    padding: 40
+  },
+  input: {
+    height: 50,
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 2,
+    textAlign: 'center'
   }
 })
